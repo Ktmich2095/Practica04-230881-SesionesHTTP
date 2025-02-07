7e8262c08f8ea5dce34ccc9bb90ee29140c87571
Ktmich2095 <230881@utxicotepec.edu.mx>
2025-02-07 12:54:08 -0600

    Practica 4
    Implementacion de modelo

--- models/sessions.model.js
+++ models/sessions.model.js
@@ -4,16 +4,25 @@ const sessionSchema = new mongoose.Schema({
     sessionId: { type: String, required: true },
     email: { type: String, required: true },
     nickname: { type: String, required: true },
-    lastAccessedAt: { type: Date, required: true },
-    clientIp: { type: String, required: true },
-    clientMac: { type: String, required: true },
-    serverIp: { type: String, required: true },
-    serverMac: { type: String, required: false },
+    createdAt:{type:Date,default:()=>moment().tz("American/Mexico_City").toDate(),required:true},
+    lastAccessedAt: { type: Date,default:()=>moment().tz("American/Mexico_City").toDate(), required: true },
     status: { 
         type: String, 
-        enum: ['activa', 'inactiva'], 
-        default: 'activa', 
+        enum: ['activa', 'inactiva','Finalizada por el usuario',"Finalizada por error del sistema"], 
         required: true 
+    },
+    clientData:{
+        ip:{type:String,required:true},
+        macAddress:{type:String,required:true}
+    },
+    serverData:{
+        ip:{type:String,required:true},
+        masAddress:{type:String,required:true}
+    },
+    inactivityTime:{
+        hours:{type:Number,required:true,min:0},
+        minutes:{type:Number,required:true,min:0,max:59},
+        seconds:{type:Number,required:true,min:0,max:59},
     }
 });
 
